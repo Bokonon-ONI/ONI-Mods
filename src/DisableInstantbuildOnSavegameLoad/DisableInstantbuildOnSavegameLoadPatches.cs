bd938b8a61a2177ac126454204bdef116d80f2a9
Bokonon-ONI <bokonongamer@gmail.com>
2019-08-22 20:23:04 -0400

    Move some things around and add DisableInstantbuildOnSavegameLoad mod

---
+++ src/DisableInstantbuildOnSavegameLoad/DisableInstantbuildOnSavegameLoadPatches.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using Harmony;
+
+namespace DisableInstantbuildOnSavegameLoad
+{
+    [HarmonyPatch(typeof(LoadScreen), "DoLoad", new [] { typeof(string) } )]
+    //[HarmonyPatch(typeof(LoadScreen))]
+    //[HarmonyPatch("Load")]
+    //[HarmonyPatch("DoLoad")]
+    internal class DisableInstantbuildOnSavegameLoadPatches
+    {
+
+        
+        public class LoadScreen_DoLoad_Patch
+        {
+            public static bool Prefix(ref DebugHandler __instance)
+            {
+                HarmonyInstance.DEBUG = true;
+
+                Harmony.FileLog.Log("Entered DisableInstantbuildOnSavegameLoad");
+                Console.WriteLine("Entered DisableInstantbuildOnSavegameLoad");
+//                DisableInstantbuildOnSavegameLoadConfig.InitLogs();
+                if (!DebugHandler.enabled)
+                    return false;
+                //if (DebugHandler.InstantBuildMode) {
+                //    DebugHandler.InstantBuildMode = false;
+                //}
+                //                (DebugHandler) __instance.InstantBuildMode = false;
+                return false;
+            }
+        }
+    }
+}
---
+++ src/InsulatedFarmTile/InsulatedFarmTilePatches.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Harmony;
+
+namespace InsulatedFarmTilePatches
+{
+	[HarmonyPatch(typeof(Game), "OnPrefabInit")]
+	internal class TemplateMod_Game_OnPrefabInit
+	{
+		private static void Postfix(Game __instance)
+		{
+			Debug.Log(" === TemplateMod_Game_OnPrefabInit Postfix === ");
+		}
+	}
+}
