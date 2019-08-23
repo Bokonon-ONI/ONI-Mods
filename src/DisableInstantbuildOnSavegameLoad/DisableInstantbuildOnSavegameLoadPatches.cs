18294ba58071ce9d63b280cddf967b5ff1fc816b
Bokonon-ONI <bokonongamer@gmail.com>
2019-08-23 13:03:48 -0400

    Working mod + cleanup crap

--- src/DisableInstantbuildOnSavegameLoad/DisableInstantbuildOnSavegameLoadPatches.cs
+++ src/DisableInstantbuildOnSavegameLoad/DisableInstantbuildOnSavegameLoadPatches.cs
@@ -1,33 +1,37 @@
 ﻿using System;
 using Harmony;
 
-namespace DisableInstantbuildOnSavegameLoad
+namespace DisableInstantBuildOnSavegameLoad
 {
-    [HarmonyPatch(typeof(LoadScreen), "DoLoad", new [] { typeof(string) } )]
-    //[HarmonyPatch(typeof(LoadScreen))]
-    //[HarmonyPatch("Load")]
-    //[HarmonyPatch("DoLoad")]
-    internal class DisableInstantbuildOnSavegameLoadPatches
+    internal class DisableInstantBuildOnSavegameLoadPatches
     {
-
-        
-        public class LoadScreen_DoLoad_Patch
+        public static class ModInfo
         {
-            public static bool Prefix(ref DebugHandler __instance)
-            {
-                HarmonyInstance.DEBUG = true;
+            public static string Name = "Disable Instant Build On Savegame Load";
+            public static string Version = "1.0.0";
+        }
 
-                Harmony.FileLog.Log("Entered DisableInstantbuildOnSavegameLoad");
-                Console.WriteLine("Entered DisableInstantbuildOnSavegameLoad");
-//                DisableInstantbuildOnSavegameLoadConfig.InitLogs();
-                if (!DebugHandler.enabled)
-                    return false;
-                //if (DebugHandler.InstantBuildMode) {
-                //    DebugHandler.InstantBuildMode = false;
-                //}
-                //                (DebugHandler) __instance.InstantBuildMode = false;
-                return false;
+        public static class OnModLoad
+        {
+            public static void OnLoad()
+            {
+                Console.WriteLine($"{Timestamp()} Loading Mod: {ModInfo.Name} Version: {ModInfo.Version}");
             }
         }
+
+        [HarmonyPatch(typeof(LoadScreen))]
+        [HarmonyPatch("OnKeyUp")]
+        public class LoadScreen_OnKeyUp_Patch
+        {
+            public static void Prefix(ref DebugHandler __instance)
+            {
+                //Console.WriteLine($"{Timestamp()} **** Entered LoadScreen_OnKeyUp_Patch.Prefix ****");
+                if (!DebugHandler.enabled)
+                    return;
+                DebugHandler.InstantBuildMode = false;
+            }
+        }
+
+        private static string Timestamp() => System.DateTime.UtcNow.ToString("[HH:mm:ss.fff]");
     }
 }
--- src/DisableInstantbuildOnSavegameLoad/Properties/AssemblyInfo.cs
+++ src/DisableInstantbuildOnSavegameLoad/Properties/AssemblyInfo.cs
@@ -5,11 +5,11 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("DisableInstantbuildOnSavegameLoad")]
+[assembly: AssemblyTitle("DisableInstantBuildOnSavegameLoad")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("Bokonon")]
-[assembly: AssemblyProduct("DisableInstantbuildOnSavegameLoad")]
+[assembly: AssemblyProduct("DisableInstantBuildOnSavegameLoad")]
 [assembly: AssemblyCopyright("Copyright ©  2019")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
